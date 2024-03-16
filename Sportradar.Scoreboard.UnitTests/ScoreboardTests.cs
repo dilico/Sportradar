efb2112253eeb527cbee92b1eace2ad79d311953
Diego Colantoni <dilico@yahoo.it>
2024-03-16 16:42:57 +0000

    Create scoreboard
    Introduced scoreboard with empty list of matches.

---
+++ Sportradar.Scoreboard.UnitTests/ScoreboardTests.cs
@@ -0,0 +1,12 @@
+﻿namespace Sportradar.Scoreboard.UnitTests;
+
+[TestClass]
+public class ScoreboardTests
+{
+  [TestMethod]
+  public void Create_ReturnsEmptyListOfMatches()
+  {
+    var scoreboard = new Scoreboard();
+    Assert.AreEqual(0, scoreboard.GetMatches().Count);
+  }
+}
---
+++ Sportradar.Scoreboard/Scoreboard.cs
@@ -0,0 +1,11 @@
+﻿namespace Sportradar.Scoreboard;
+
+public class Scoreboard
+{
+  private IList<Match> Matches { get; } = [];
+
+  public IList<Match> GetMatches()
+  {
+    return Matches;
+  }
+}
