e9873250816f98ea7b58f2f27755867a7882be30
Diego Colantoni <dilico@yahoo.it>
2024-03-16 09:24:56 +0000

    Create teams
    Introduced teams defined only by their name. The team name must not
    be null or including only whitespaces.

---
+++ Sportradar.Scoreboard.UnitTests/TeamTests.cs
@@ -0,0 +1,27 @@
+﻿namespace Sportradar.Scoreboard.UnitTests;
+
+[TestClass]
+public class TeamTests
+{
+  [TestMethod]
+  [DataRow("West Ham", "West Ham")]
+  [DataRow(" West Ham", "West Ham")]
+  [DataRow("West Ham ", "West Ham")]
+  [DataRow(" West Ham ", "West Ham")]
+  [DataRow("  West Ham  ", "West Ham")]
+  public void Team_Name(string name, string expected)
+  {
+    var team = new Team(name);
+    Assert.AreEqual(expected, team.Name);
+  }
+
+  [TestMethod]
+  [ExpectedException(typeof(ArgumentException))]
+  [DataRow(" ", DisplayName = "Team name with only whitespaces")]
+  [DataRow("", DisplayName = "Empty team name")]
+  [DataRow(null, DisplayName = "Null team name")]
+  public void Team_WithInvalidName_ThrowsArgumentException(string name)
+  {
+    _ = new Team(name);
+  }
+}
---
+++ Sportradar.Scoreboard/Team.cs
@@ -0,0 +1,15 @@
+﻿namespace Sportradar.Scoreboard;
+
+public class Team
+{
+  public string Name { get; }
+
+  public Team(string name)
+  {
+    if (string.IsNullOrWhiteSpace(name))
+    {
+      throw new ArgumentException("Team name cannot be empty");
+    }
+    Name = name.Trim();
+  }
+}
